b8aaf018338c102fb1c57cd8cd973602d463e1e6
Nicolas Landais <nicolas.landais@citrix.com>
2019-01-01 12:24:27 -0500

    Single pipeline

--- new_pipeline.groovy
+++ new_pipeline.groovy
@@ -1,32 +1,8 @@
-
-// define the repos we want to build on CI
-def repos = [ 'jenkins-pipes-helloworld' ]
-
-// create a multibranch pipeline job for each of the repos
-for (repo in repos)
-{
-  multibranchPipelineJob("${repo}") {
-
-    // build master as well as feature branches
-    branchSources {
-      git {
-        remote("https://github.com/tknerr/${repo}.git")
-        includes("master feature/*")
-      }
+pipelineJob('example') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('Jenkinsfile'))
+            sandbox()
+        }
     }
-    // check every minute for scm changes as well as new / deleted branches
-    triggers {
-      periodic(1)
-    }
-    // don't keep build jobs for deleted branches
-    orphanedItemStrategy {
-      discardOldItems {
-        numToKeep(0)
-      }
-    }
-  }
-  // automatically queue the job after the initial creation
-  if (!jenkins.model.Jenkins.instance.getItemByFullName("${repo}")) {
-    queue("${repo}")
-  }
 }
