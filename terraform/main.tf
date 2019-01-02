df873aba921f5f80756d75625190a87f6a89333b
Nicolas Landais <nicolas.landais@citrix.com>
2019-01-02 11:41:25 -0500

    Add Terraform provisioning to run Jenkins on Docker on an Ubuntu EC2 instance.

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "jenkins"
+  description = "Used to secure the Jenkins instance"
+  vpc_id      = "${var.aws_vpc}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}"]
+  }
+
+  ingress {
+    from_port   = 2222
+    to_port     = 2222
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["${var.my_ip}"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+  }
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+  key_name = "${var.ssh_key_id}"
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id = "${var.aws_subnet}"
+
+  # Provisioner the instance and run admin/install tasks.
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'",
+      "sudo apt update",
+      "apt-cache policy docker-ce",
+      "sudo apt install -y docker-ce",
+      "sudo gpasswd -a $USER docker"
+    ]
+  }
+
+  # Create the Docker image and start Jenkins.
+  provisioner "remote-exec" {
+    inline = [
+      "git clone https://github.com/nlandais/Jenkins.git",
+      "cd Jenkins",
+      "docker build -t nic-jenkins:latest .",
+      "docker run -d --name nic-jenkins -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home nic-jenkins:latest"
+    ]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,40 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
+  default = "~/.ssh/<redacted>"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "my_ip" {
+  default = "<redacted>"
+}
+
+variable "aws_vpc" {
+  default = "<redacted>"
+}
+
+variable "aws_subnet" {
+  default = "<redacted>"
+}
+
+variable "ssh_key_id" {
+  default = "<redacted>"
+}
+
+# Ubuntu Precise 18.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    us-east-1 = "ami-0ac019f4fcb7cb7e6"
+    us-west-1 = "ami-063aa838bd7631e0b"
+    us-west-2 = "ami-0bbe6b35405ecebdb"
+  }
+}
