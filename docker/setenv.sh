d0adf50b979e56a6ca3614b01fa7f1d69bfa9b06
Nicolas Landais <nicolas.landais@citrix.com>
2019-06-06 16:50:51 -0400

    Removing deprcated script and cleaning up comments

--- docker/setenv.sh
+++ docker/setenv.sh
@@ -9,11 +9,3 @@ export JAVA_OPTS="-Djenkins.install.runSetupWizard=false -DJENKINS_HOME=${JENKIN
 export JENKINS_UC="https://updates.jenkins.io"
 export JENKINS_UC_EXPERIMENTAL="https://updates.jenkins.io/experimental"
 export JENKINS_INCREMENTALS_REPO_MIRROR="https://repo.jenkins-ci.org/incrementals"
-#
-# # Create ENV variables used by scripts/groovy downstream
-# grep -q JENKINS_SLAVE_AGENT_PORT /etc/environment || echo JENKINS_SLAVE_AGENT_PORT=${agent_port} >> /etc/environment
-# grep -q COPY_REFERENCE_FILE_LOG /etc/environment || echo COPY_REFERENCE_FILE_LOG=${JENKINS_HOME}/copy_reference_file.log >> /etc/environment
-# grep -q JAVA_OPTS /etc/environment || echo 'JAVA_OPTS="-Djenkins.install.runSetupWizard=false -DJENKINS_HOME=${JENKINS_HOME}"' >> /etc/environment
-# grep -q JENKINS_UC /etc/environment || echo JENKINS_UC=https://updates.jenkins.io  >> /etc/environment
-# grep -q JENKINS_UC_EXPERIMENTAL /etc/environment || echo JENKINS_UC_EXPERIMENTAL=https://updates.jenkins.io/experimental  >> /etc/environment
-# grep -q JENKINS_INCREMENTALS_REPO_MIRROR /etc/environment || echo JENKINS_INCREMENTALS_REPO_MIRROR=https://repo.jenkins-ci.org/incrementals >> /etc/environment
